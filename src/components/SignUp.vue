67b0d08329def201bce87609df5501ec70955c99
jihye_son <jiiihyeee98@gmail.com>
2022-01-28 10:57:00 +0900

    sns회원가입 아이콘과 구분선 넣음

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
-    <div>hi</div>
+    <h1 class="signup" style="margin-top: 50px">회원가입</h1>
+    <p style="margin-top: 50px">SNS계정으로 간편하게 회원가입</p>
+    <span
+      ><i
+        style="margin-right: 30px; margin-top: 30px"
+        class="fab fa-facebook-square"
+      ></i
+    ></span>
+    <span><i style="margin-right: 30px" class="fab fa-kickstarter-k"></i></span>
+    <span><i class="fab fa-instagram"></i></span>
+    <hr width="300px" style="margin-top: 30px" />
   </div>
 </template>
 
