02a1087b6fc984b0e8c3f0e4c5c8631247b6859c
jihye_son <jiiihyeee98@gmail.com>
2022-05-10 14:57:34 +0900

    행마다 금액 가져온 뒤 합계 계산 구현완료

--- src/components/User/NewTable.vue
+++ src/components/User/NewTable.vue
@@ -26,16 +26,38 @@
     <td>
       <input
         type="text"
-        id="name"
+        class="mm"
         value="0"
         style="border:0 solid black; width:100%"
+        @blur="message()"
       />
     </td>
   </tr>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    sum: 0;
+    nums: [];
+  },
+  methods: {
+    message() {
+      const value = document.querySelectorAll('.mm');
+      this.nums = [];
+      this.sum = 0;
+      for (var i = 0; i < value.length; i++) {
+        console.log(value[i].value);
+        this.nums.push(value[i].value);
+        console.log('숫자들', this.nums);
+      }
+      this.nums.forEach((item) => {
+        this.sum += parseInt(item);
+        console.log('합계', this.sum);
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
