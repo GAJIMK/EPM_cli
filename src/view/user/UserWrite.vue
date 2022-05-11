04162f4e0dbdf0184edd5e5a90c52357242dd38b
jihye_son <jiiihyeee98@gmail.com>
2022-05-11 16:22:10 +0900

    img container css 수정

--- src/view/user/UserWrite.vue
+++ src/view/user/UserWrite.vue
@@ -1,103 +1,70 @@
 <template>
-  <div id="app">
-    <div class="card">
-      <div class="card-body">
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-1 col-form-label"
-            >제목 :
-          </label>
-          <div class="col-sm-10">
-            <input
-              type="month"
-              class="caption form-control"
-              id="Title"
-              placeholder="제목을 입력하세요 ( 2022-04 형태로 입력할 것 )"
-              max="9999-12"
-            />
-          </div>
-        </div>
-
-        <hr />
-        <d-row>
-          <span>&lt;팀활동비></span>
-          <button class="plusRow BtnStyle" @click="add()">행추가</button>
-          <button class="BtnStyle" @click="deleteRow(-1)">행삭제</button>
-        </d-row>
-
-        <div class="imdiv">
-          <table
-            contenteditable="true"
-            id="table_1"
-            class="table_1 table-bordered table-hover"
-            v-bind="this.items"
-          >
-            <thead class="thead_1" id="thead_1">
-              <tr class="select">
-                <td>
-                  <input
-                    class="thead"
-                    type="text"
-                    disabled="true"
-                    value="일자"
-                  />
-                </td>
-                <td>
-                  <input
-                    class="thead"
-                    type="text"
-                    disabled="true"
-                    value="내역"
-                  />
-                </td>
-                <td>
-                  <input
-                    class="thead"
-                    type="text"
-                    disabled="true"
-                    value="장소"
-                  />
-                </td>
-                <td>
-                  <input
-                    class="thead"
-                    type="text"
-                    disabled="true"
-                    value="동반인"
-                  />
-                </td>
-                <td>
-                  <input
-                    class="thead"
-                    type="text"
-                    disabled="true"
-                    value="개인카드/현금"
-                  />
-                </td>
-                <td>
-                  <input
-                    class="thead"
-                    type="text"
-                    disabled="true"
-                    value="금액"
-                  />
-                </td>
-              </tr>
-            </thead>
-            <tbody class="tbody_1">
-              <!-- <newTable @message="updateSum" /> -->
-              <newTable
-                v-for="item in items"
-                :key="item.id"
-                @message="updateSum"
-              />
-            </tbody>
-          </table>
-        </div>
-        <div class="ShowMoney" id="ShowMoney">합계 : {{ showsum }}</div>
-        <div class="leftMoney" id="leftMoney">남은 금액 : {{ leftmeoney }}</div>
-
-        <span> <ImgUpload v-for="img in imgs" :key="img.id"></ImgUpload></span>
+  <div class="app">
+    <div class="form-group row">
+      <label for="inputPassword" class="col-sm-1 col-form-label">제목 : </label>
+      <div class="col-sm-10">
+        <input
+          type="month"
+          class="caption form-control"
+          id="Title"
+          placeholder="제목을 입력하세요 ( 2022-04 형태로 입력할 것 )"
+          max="9999-12"
+        />
       </div>
+    </div>
+
+    <hr />
+    <d-row>
+      <span>&lt;팀활동비></span>
+      <button class="plusRow BtnStyle" @click="add()">행추가</button>
+      <button class="BtnStyle" @click="deleteRow(-1)">행삭제</button>
+    </d-row>
+
+    <div class="imdiv">
+      <table
+        contenteditable="true"
+        id="table_1"
+        class="table_1 table-bordered table-hover"
+        v-bind="this.items"
+      >
+        <thead class="thead_1" id="thead_1">
+          <tr class="select">
+            <td>
+              <input class="thead" type="text" disabled="true" value="일자" />
+            </td>
+            <td>
+              <input class="thead" type="text" disabled="true" value="내역" />
+            </td>
+            <td>
+              <input class="thead" type="text" disabled="true" value="장소" />
+            </td>
+            <td>
+              <input class="thead" type="text" disabled="true" value="동반인" />
+            </td>
+            <td>
+              <input
+                class="thead"
+                type="text"
+                disabled="true"
+                value="개인카드/현금"
+              />
+            </td>
+            <td>
+              <input class="thead" type="text" disabled="true" value="금액" />
+            </td>
+          </tr>
+        </thead>
+        <tbody class="tbody_1">
+          <!-- <newTable @message="updateSum" /> -->
+          <newTable v-for="item in items" :key="item.id" @message="updateSum" />
+        </tbody>
+      </table>
+    </div>
+    <div class="ShowMoney" id="ShowMoney">합계 : {{ showsum }}</div>
+    <div class="leftMoney" id="leftMoney">남은 금액 : {{ leftmeoney }}</div>
+
+    <div class="fluid-container" id="billimg">
+      <ImgUpload class="item" v-for="img in imgs" :key="img.id"></ImgUpload>
     </div>
   </div>
 </template>
@@ -212,6 +179,11 @@ export default {
 .new {
   background-color: #e3ffc8;
 }
-.ShowMoney .leftMoney {
+.fluid-container {
+  display: flex;
+  flex-wrap: wrap;
+
+  justify-content: space-around;
+  width: 100%;
 }
 </style>
