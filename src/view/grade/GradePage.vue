7190afa4bd764b0cff761f3c37010532778cbb62
gajeong <rlarkwjd127@gmail.com>
2022-07-06 18:29:34 +0900

    api 데이터 출력

--- src/view/grade/GradePage.vue
+++ src/view/grade/GradePage.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="container">
     <MenuTitle menuTitle="👨‍👦‍👦 등급별 경비 관리" />
-    <div class="cols">
-      <div class="name">이름</div>
+    <div class="contents">
+      <section class="position">
+        <div class="pos" v-for="pos in positions" :key="pos.commonCode">
+          {{ pos.commonName }}
+        </div>
+      </section>
+      <section class="expenses">
+        <div class="col" v-for="exp in expenses" :key="exp.summCode">
+          {{ exp.summCodeName }}
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -10,17 +19,39 @@
 <script>
 import MenuTitle from '@/components/common/MenuTitleForm.vue';
 import { fetchPosition } from '@/api/common/commonCode';
+import { fetchExpense } from '@/api/expense/expense';
 export default {
   components: { MenuTitle },
+  mounted() {
+    this.fetchPosition();
+    this.fetchExpense();
+  },
+  data() {
+    return {
+      positions: [],
+      expenses: [],
+    };
+  },
+  methods: {
+    async fetchPosition() {
+      const res = await fetchPosition();
+      this.positions = res.data.list;
+    },
+    async fetchExpense() {
+      const res = await fetchExpense();
+      this.expenses = res.data.list;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.cols {
+.expenses {
   display: flex;
   color: #999;
-  font-size: 12px;
+  font-size: 14px;
   padding-bottom: 4px;
   border-bottom: solid #eee 0.5px;
+  text-align: center;
 }
 </style>
