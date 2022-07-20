19d577f518f7c9574dd808a125cd27acccd812d4
jihye_son <jiiihyeee98@gmail.com>
2022-07-20 16:58:54 +0900

    주석제거

--- src/components/ToastMsgg.vue
+++ src/components/ToastMsgg.vue
@@ -44,26 +44,6 @@ export default {
     createToast() {
       this.showBottom = true;
     },
-
-    // showToast() {
-    //   // Use a shorter name for this.$createElement
-    //   const h = this.$createElement;
-    //   // Create the message
-    //   const vNodesMsg = h('p', { class: ['text-center', 'mb-0'] }, [
-    //     h('span', this.ToastCon),
-    //     ,
-    //   ]);
-    //   // Create the title
-
-    //   // Pass the VNodes as an array for message and title
-    //   this.$bvToast.toast([vNodesMsg], {
-    //     solid: false,
-    //     variant: 'warning',
-    //     autoHideDelay: 1500,
-    //     toaster: 'b-toaster-bottom-center',
-    //     static: true,
-    //   });
-    // },
   },
 };
 </script>
