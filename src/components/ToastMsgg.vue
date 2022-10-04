842187b320fc034786f468c012af3443ddd20683
jihye_son <jiiihyeee98@gmail.com>
2022-10-04 15:09:55 +0900

    toast msg

--- src/components/ToastMsgg.vue
+++ src/components/ToastMsgg.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="toastContainer">
-
-<<<<<<< HEAD
     <b-toast
       id="b-toaster-bottom-center"
       variant="warning"
@@ -9,10 +7,6 @@
       v-model="show"
       toaster="b-toaster-bottom-center"
     >
-=======
-    <b-toast id="b-toaster-bottom-center" variant="warning" solid v-model="showBottom"
-      toaster="b-toaster-bottom-center">
->>>>>>> e3b8738a4c8ebbf7a1fa54c196961988224a4cd9
       <template #toast-title>
         <div class="d-flex flex-grow-1 align-items-baseline">
           <b-img blank blank-color="#ff5555" class="mr-2" width="12" height="12"></b-img>
